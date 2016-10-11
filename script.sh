ca23811cc9b0e952ef210e9fd406de6fcdcda2e1
Sierra Yit <sierrayit@gmail.com>
2016-10-11 14:38:07 -0400

    changed to .z for gunzip

--- script.sh
+++ script.sh
@@ -3,9 +3,9 @@
 # cutoff = $1
 
 # download file and sort it
-curl -o pagecounts-20160801-000000.gz "https://dumps.wikimedia.org/other/pagecounts-raw/2016/2016-08/pagecounts-20160801-000000.gz"
-gunzip pagecounts-20160801-000000.gz
-grep ^en pagecounts-20160801-000000 > en.pagecounts
+curl -o pageviews-20161011-160000.z "https://dumps.wikimedia.org/other/pageviews/2016/2016-10/pageviews-20161011-160000.gz"
+gunzip pageviews-20161011-160000.z
+grep ^en pageviews-20161011-160000 > en.pagecounts
 cut -f4 -d" " en.pagecounts | paste - en.pagecounts | sort -nr > en.pagecounts.sorted
 
 # iterate through and eliminate pages with under # views
