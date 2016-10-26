ec6a606063ea669f0c37c6a9268d5474f477b4f2
Sierra Yit <sierrayit@gmail.com>
2016-10-26 01:03:02 -0400

    script to parse pagecount files to csv dicts

---
+++ makeDict.py
@@ -0,0 +1,21 @@
+import sys
+import csv
+
+
+def main():
+  f = open(sys.argv[1])
+  output = open('output.csv', 'w+')
+  headers = []
+  d = {}  
+  for line in f:
+    s = line.split()
+    if s[1][0] != '%' and ":" not in s[1]:
+      d[s[1]] = int(s[len(s) - 1])
+      headers.append(s[1])
+  #print d
+  writer = csv.DictWriter(output, fieldnames=headers)
+  writer.writeheader()
+  writer.writerow(d)
+
+if __name__ == '__main__':
+  main()
