d836e06231537f02d2f682328a6666af13e87d7b
cristinabuenahora <crisb@seas.upenn.edu>
2016-10-26 01:57:33 -0400

    fixed makeDict.py

--- makeDict.py
+++ makeDict.py
@@ -5,17 +5,19 @@ import csv
 def main():
   f = open(sys.argv[1])
   output = open('output.csv', 'w+')
-  headers = []
-  d = {}  
+  headers = ['page', 'pageviews'] 
+  writer = csv.DictWriter(output, fieldnames=headers)
+  writer.writeheader()
   for line in f:
     s = line.split()
     if s[1][0] != '%' and ":" not in s[1]:
-      d[s[1]] = int(s[len(s) - 1])
-      headers.append(s[1])
+      writer.writerow({'page': s[1], 'pageviews': s[len(s) - 1]})
+      #d[s[1]] = int(s[len(s) - 1])
+      #headers.append(s[1])
   #print d
-  writer = csv.DictWriter(output, fieldnames=headers)
-  writer.writeheader()
-  writer.writerow(d)
+  #writer = csv.DictWriter(output, fieldnames=headers)
+  #writer.writeheader()
+  #writer.writerow(d)
 
 if __name__ == '__main__':
   main()
--- shrink.py
+++ shrink.py
@@ -1,13 +1,13 @@
 def main():
-	f1 = open('en.pagecounts1')
-	f2 = open('en.pagecounts2')
+	f1 = open(sys.argv[1])
+	f2 = open(sys.argv[2])
 	for line1, line2 in zip(f1, f2):
 		s1 = line1.split()
 		s2 = line2.split()
 		count1 = int(s1[len(s1)-1])
 		count2 = int(s2[len(s1)-1])
 		dif = abs(count1 - count2)
-		if (count1 > 10000 or count2 > 10000 or dif > 500):
+		if (count1 > 10000 or count2 > 10000 or dif > 500):`
 			print "%d, %d, %d, %s" % (count1, count2, dif, s1[1])
 
 	#for line1 in f1:
