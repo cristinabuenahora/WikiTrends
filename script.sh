1336e084ac05e22084d64cc5701fb13f2d036957
cristinabuenahora <crisb@seas.upenn.edu>
2016-11-02 11:25:08 -0400

    minor change for script.sh

--- script.sh
+++ script.sh
@@ -11,9 +11,3 @@ curl -o $1.gz $url
 gunzip $1.gz
 grep ^en $1 > en.pagecounts1
 #cut -f4 -d" " en.pagecounts1 | paste - en.pagecounts1 | sort -nr > en.pagecounts.sorted1
-
-# download file and sort it
-#curl -o pagecounts-20160716-000000.gz "https://dumps.wikimedia.org/other/pageviews/2016/2016-11/pagecounts-20161101-000000.gz"
-#gunzip pagecounts-20160716-000000.gz
-#grep ^en pagecounts-20160716-000000 > en.pagecounts2
-#cut -f4 -d" " en.pagecounts2 | paste - en.pagecounts2 | sort -nr > en.pagecounts.sorted2
