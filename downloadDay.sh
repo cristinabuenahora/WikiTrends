8bcfbb01802f7242babf5783a3314bd5b3c55865
Sierra Yit <sierrayit@gmail.com>
2016-11-07 12:00:49 -0500

    download day takes in month and writes to diff directories

--- downloadDay.sh
+++ downloadDay.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
+# Usage: bash downloadDay.sh 10 16
+# arg 1 is the month
+# arg 2 is the day
+
+fullfile="pageviews-2016"$1$2
 
 # download 12:00AM file and get english pages
-url="https://dumps.wikimedia.org/other/pageviews/2016/2016-10/"$1.gz
-curl -o $1.gz $url
-gunzip $1.gz
-grep ^en $1 > en.$1
+file=$fullfile-000000
+url="https://dumps.wikimedia.org/other/pageviews/2016/2016-"$1/$file.gz
+curl -o "pageviews/"$file.gz $url
+gunzip "pageviews/"$file.gz
+grep ^en "pageviews/"$file > "enpageviews/"en.$file
 
 # download 6:00AM file and get english pages
-file=${1::19}060000
-url="https://dumps.wikimedia.org/other/pageviews/2016/2016-10/"$file.gz
-curl -o $file.gz $url
-gunzip $file.gz
-grep ^en $file > en.$file
+file=$fullfile-060000
+url="https://dumps.wikimedia.org/other/pageviews/2016/2016-"$1/$file.gz
+curl -o "pageviews/"$file.gz $url
+gunzip "pageviews/"$file.gz
+grep ^en "pageviews/"$file > "enpageviews/"en.$file
+
 
 # download 12:00PM file and get english pages
-file=${1::19}120000
-url="https://dumps.wikimedia.org/other/pageviews/2016/2016-10/"$file.gz
-curl -o $file.gz $url
-gunzip $file.gz
-grep ^en $file > en.$file
+file=$fullfile-120000
+url="https://dumps.wikimedia.org/other/pageviews/2016/2016-"$1/$file.gz
+curl -o "pageviews/"$file.gz $url
+gunzip "pageviews/"$file.gz
+grep ^en "pageviews/"$file > "enpageviews/"en.$file
 
 # download 6:00PM file and get english pages
-file=${1::19}180000
-url="https://dumps.wikimedia.org/other/pageviews/2016/2016-10/"$file.gz
-curl -o $file.gz $url
-gunzip $file.gz
-grep ^en $file > en.$file
+file=$fullfile-180000
+url="https://dumps.wikimedia.org/other/pageviews/2016/2016-"$1/$file.gz
+curl -o "pageviews/"$file.gz $url
+gunzip "pageviews/"$file.gz
+grep ^en "pageviews/"$file > "enpageviews/"en.$file
+
