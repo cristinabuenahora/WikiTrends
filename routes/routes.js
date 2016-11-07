6a5dd679eeaf7f72c0e6fc326df18d622ac26689
cristinabuenahora <crisb@seas.upenn.edu>
2016-11-07 12:17:52 -0500

    basic webpage reading in from textfile

--- routes/routes.js
+++ routes/routes.js
@@ -1,26 +1,20 @@
+var fs = require('fs'); 
+
 module.exports = function(app) {
 
 	// application -------------------------------------------------------------
 	app.get('/', function(req, res) {
+			var pageNames = fs.readFileSync('test.txt').toString('utf8').split('\n');
+			var numPages = pageNames.length;
 
-		var lineReader = require('readline').createInterface({
-			input: require('fs').createReadStream('../test.txt')
-		});
-
-		var wikiPages = new Object();
-		lineReader.on('line', function (line) {
-			wikiPages.push({name: line});
-			console.log('Line from file:', line);
-		});
-
-		res.render('../views/index.ejs', {
-			wikiPages: wikiPages
-		});
-
+			res.render('index', {
+					wikiPageNames: pageNames,
+					numberOfPages: numPages,
+			});
 	});
 
 	app.get('/about', function(req, res) {
-		res.sendfile('../views/about.ejs');
+		res.render('about.ejs');
 	});
 
 };
