5f10ac57e0bf40ce018d5f6119e27e52e5308bc4
Sierra Yit <sierrayit@gmail.com>
2016-11-11 17:11:32 -0500

    changed to using wget

--- downloadDay.sh
+++ downloadDay.sh
@@ -8,14 +8,14 @@ fullfile="pageviews-2016"$1$2
 # download 12:00AM file and get english pages
 file=$fullfile-000000
 url="https://dumps.wikimedia.org/other/pageviews/2016/2016-"$1/$file.gz
-curl -o "pageviews/"$file.gz $url
+wget $url -O "pageviews/"$file.gz
 gunzip "pageviews/"$file.gz
 grep ^en "pageviews/"$file > "enpageviews/"en.$file
 
 # download 6:00AM file and get english pages
 file=$fullfile-060000
 url="https://dumps.wikimedia.org/other/pageviews/2016/2016-"$1/$file.gz
-curl -o "pageviews/"$file.gz $url
+wget $url -O "pageviews/"$file.gz 
 gunzip "pageviews/"$file.gz
 grep ^en "pageviews/"$file > "enpageviews/"en.$file
 
@@ -23,14 +23,14 @@ grep ^en "pageviews/"$file > "enpageviews/"en.$file
 # download 12:00PM file and get english pages
 file=$fullfile-120000
 url="https://dumps.wikimedia.org/other/pageviews/2016/2016-"$1/$file.gz
-curl -o "pageviews/"$file.gz $url
+wget $url -O "pageviews/"$file.gz 
 gunzip "pageviews/"$file.gz
 grep ^en "pageviews/"$file > "enpageviews/"en.$file
 
 # download 6:00PM file and get english pages
 file=$fullfile-180000
 url="https://dumps.wikimedia.org/other/pageviews/2016/2016-"$1/$file.gz
-curl -o "pageviews/"$file.gz $url
+wget $url -O "pageviews/"$file.gz
 gunzip "pageviews/"$file.gz
 grep ^en "pageviews/"$file > "enpageviews/"en.$file
 
