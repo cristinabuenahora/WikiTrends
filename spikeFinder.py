10a887cc4e2dde409a89ebcb82d94b4452dd9e3d
Sierra Yit <sierrayit@gmail.com>
2016-11-19 11:41:23 -0500

    basic spikeFinder

--- spikeFinder.py
+++ spikeFinder.py
@@ -1,31 +1,40 @@
 
 import sys
+import math
+
+'''
+takes in makeDict.py data of 
+pagename  day1 day2 day3
+'''
 
 def main():
-  files = []
-  for i in range(1,len(sys.argv)):
-    files.append(sys.argv[i])
-  
-
-  dic = {}
-  for f in files:
-    diff = open(f)
-    for line in diff:
-      s = line.split('\t')
-      page = s[1].strip()
-      count = s[0].strip()
-      if page[0] != '\\':
-        if page in dic:
-          dic[page].append(count)
-        else:
-          dic[page] = [count]
-  
-  for page in dic:
-    count = 0
-    for num in dic[page]:
-      count += int(num)
-    print str(count) + '\t' + page
-
+  f = open(sys.argv[1])
+  for line in f:
+    # put the dayviews in a list
+    tab = line.split('\t')
+    pagename = tab[0]
+    if (len(tab) > 1):
+      counts = tab[1].split()
+      if (len(counts) > 3):
+        
+        # average the first two thirds of the list
+        avg1 = 0
+        idx = int(math.floor(len(counts) * (2.0/3)))
+        for i in range(0, idx):
+          avg1 += int(counts[i])
+        avg1 = avg1/idx
+      
+        # average the last third of the list
+        avg2 = 0
+        for i in range(idx, len(counts)):
+          avg2 += int(counts[i])
+        avg2 = avg2/(len(counts) - idx)
+          
+        # check if the difference is above a certain percent change and actual value
+        diff = avg2 - avg1
+        if (diff > avg1 * 1.5 and diff > 500):
+          # if it is then print the name and the difference
+          print pagename + '\t' + str(diff)
 
 if __name__ == '__main__':
   main()
