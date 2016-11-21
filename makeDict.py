ec9b5991f9837b3e3a46d8d417570693034eadef
Sierra Yit <sierrayit@gmail.com>
2016-11-21 13:59:56 -0500

    removes pages with less than 100 views

--- makeDict.py
+++ makeDict.py
@@ -1,4 +1,3 @@
-
 import sys
 import os
 
@@ -10,8 +9,12 @@ should be captured in a file in that directory
 
 def main():
  
+  # dictionary of counts
+  # { 'title': [count1,count2,....], 'title2': [count1, count2...] }
   d = {}
+  currDay = 0
   for filename in os.listdir(sys.argv[1]):
+    currDay += 1
     f = open(sys.argv[1] + "/" + filename)
     seen = {}
     for line in f:
@@ -19,8 +22,12 @@ def main():
       try:
         title = splits[1].strip()
         views = splits[2].strip()
+
+        # pad the list of counts with the zeros for missed days
         if title not in d:
           d[title] = []
+        
+        # account for duplicates of a title in one file
         if title not in seen:
           d[title].append(views)
           seen[title] = True
@@ -28,16 +35,23 @@ def main():
           l = len(d[title]) - 1
           currCount = d[title][l]
           d[title][l] = str(int(currCount) + int(views))
+
       except:
         x = 1
 
-
   s = ''
   for title in d:
-    s += title + '\t'
+    keep = False
+    t = title + '\t'
     for count in d[title]:
-      s += count + ' '
-    s += '\n'
+      t += count + ' '
+      try:
+        if (int(count) > 100):
+          keep = True
+      except:
+          x = 1
+    if keep:
+      s += t + '\n'
   
   print s
 
