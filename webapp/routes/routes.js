be7a74a206524e62d7d522e1efcee7d3e96320f1
cristinabuenahora <crisb@seas.upenn.edu>
2017-02-01 14:19:18 -0500

    removed election day page

--- webapp/routes/routes.js
+++ webapp/routes/routes.js
@@ -6,7 +6,7 @@ module.exports = function(app) {
 
 	app.get('/', function(req, res) {
 			var data = fs.readFileSync('data/test.txt').toString('utf8').split('\n');
-			var pageNames = []; 
+			var pageNames = [];
 			for (var i = 0; i < data.length; i++) {
 				var pageName = data[i].split('\t')[0];
 				pageName = pageName.split('_').join(' ');
@@ -20,22 +20,6 @@ module.exports = function(app) {
 			});
 	});
 
-	app.get('/electionday', function(req, res) {
-			var data = fs.readFileSync('data/oldtest.txt').toString('utf8').split('\n');
-			var pageNames = []; 
-			for (var i = 0; i < data.length; i++) {
-				var pageName = data[i].split('\t')[0];
-				pageName = pageName.split('_').join(' ');
-				pageNames.push(pageName);
-			}
-			var numPages = pageNames.length;
-
-			res.render('electionIndex', {
-					wikiPageNames: pageNames,
-					numberOfPages: numPages,
-			});
-	});
-
 	app.get('/about', function(req, res) {
 		res.render('about.ejs');
 	});
@@ -44,17 +28,8 @@ module.exports = function(app) {
 		res.render('wikipage.ejs');
 	});
 
-	app.get('/electionWikipage', function(req, res) {
-		res.render('electionWikipage.ejs');
-	});
-
 	app.get('/getData', function(req, res){
 		var pageNames = fs.readFileSync('data/test.txt').toString('utf8').split('\n');
-		res.json({ pageData: pageNames });
-	});
-
-	app.get('/getElectionData', function(req, res){
-		var pageNames = fs.readFileSync('data/oldtest.txt').toString('utf8').split('\n');
 		res.json({ pageData: pageNames });
 	});
 
