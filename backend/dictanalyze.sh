4caac080d30fb6612f1db932c7bc29190cff77d8
Sierra Yit <sierrayit@gmail.com>
2017-02-05 12:07:48 -0500

    added dictanalyze and hourly script

---
+++ backend/dictanalyze.sh
@@ -0,0 +1,10 @@
+
+month="01"
+year="2017"
+python makeDict.py "/nlp/data/sierray/"$month"1"$year > "/nlp/data/sierray/"$month"1"$year"/countDict"
+
+python makeDict.py "/nlp/data/sierray/"$month"2"$year > "/nlp/data/sierray/"$month"2"$year"/countDict"
+
+python makeDict.py "/nlp/data/sierray/"$month"3"$year > "/nlp/data/sierray/"$month"3"$year"/countDict"
+
+python makeDict.py "/nlp/data/sierray/"$month"4"$year > "/nlp/data/sierray/"$month"4"$year"/countDict"
