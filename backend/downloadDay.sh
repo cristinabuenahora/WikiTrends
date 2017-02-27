c4eaff4f4d152e988e651afded8c0690a6fae701
Sierra Yit <sierrayit@gmail.com>
2017-02-27 14:17:11 -0500

    easier to change dirs

--- backend/downloadDay.sh
+++ backend/downloadDay.sh
@@ -7,13 +7,16 @@
 month=$1
 day=$2
 week=$3
+zippedDir="/nlp/data/sierray/pageviews/"
+unzippedDir="/nlp/data/sierray/"$month$week"2017/"
+
 hours=("00" "01" "02" "03" "04" "05" "06" "07" "08" "09" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "20" "21" "22" "23")
 for hour in ${hours[*]}
 do
   file="pageviews-2017"$month$day"-"$hour"0000"
   url="https://dumps.wikimedia.org/other/pageviews/2017/2017-"$month"/"$file.gz
-  wget $url -O "zipped/"$file.gz
-  gunzip "zipped/"$file.gz
-  grep ^en "zipped/"$file > "data/"$month$week"2017/"$file
-  rm "zipped/"$file
+  wget $url -O $zippedDir$file.gz
+  gunzip $zippedDir$file.gz
+  grep ^en $zippedDir$file > $unzippedDir$file
+  rm $zippedDir$file
 done
