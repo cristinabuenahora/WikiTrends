f51f68fe9e0382fd8614d4ba089285e1fb768ddc
cristinabuenahora <crisb@seas.upenn.edu>
2017-04-02 19:15:48 -0400

    minor modification to category alg

--- backend/category.py
+++ backend/category.py
@@ -10,10 +10,10 @@ def main():
     f = open(sys.argv[1])
     out = open(sys.argv[2], 'w')
 
-    politics = ['geography', 'law', 'politic', 'government', 'president', 'congress', 'judge']
+    politics = ['geography', 'law', 'politic', 'government', 'president', 'congress', 'judge', 'leader']
     history = ['history', 'war', 'philosophy', 'society', 'holiday', 'century', 'bomb']
-    techScience = ['industry', 'tech', 'health', 'science', 'matter', 'nature', 'disease', 'code', 'math', 'algebra']
-    sports = ['sport', 'game', 'athelete', 'basketball', 'football', 'soccer', 'baseball', 'tennis', 'fifa', 'boxing', 'run']
+    techScience = ['industry', 'tech', 'health', 'science', 'matter', 'nature', 'disease', 'code', 'math', 'graph', 'algebra', 'environment', 'carbon']
+    sports = ['sport', 'athelete', 'basketball', 'football', 'soccer', 'baseball', 'tennis', 'fifa', 'boxing', 'run']
     entertainment = ['arts', 'culture', 'entertainment', 'movie', 'actor', 'actress', 'novel', 'television', 'film', 'album', 'music', 'animate', 'animal', 'comic', 'video']
 
     #list = ['arts', 'culture', 'entertainment', 'game', 'geography', 'health', 'history', 'humanities', 'industry', 'law', 'life', 'math', 'matter', 'nature', 'philsophy', 'people', 'politic', 'reference work', 'religion', 'science', 'technology', 'society']
@@ -29,12 +29,15 @@ def main():
         pagename = pagename.replace('_', ' ')
         categories = line.split('\t')[3].split(',')
         count = numpy.zeros(len(list))
+        print pagename
+        print categories
 
         for c in categories:
             for i in range(0, len(count)):
                 for l in list[i]:
                     if l in c.lower():
                         count[i] = count[i] + 1
+                        print l
 
         max = 0
         main = []
@@ -47,8 +50,12 @@ def main():
                 max = count[i]
                 main.append(listName[i])
 
+        print '----------------------'
+        print main
         newline = line.split('\t')[0] + '\t' + line.split('\t')[1] + '\t' + line.split('\t')[2] + '\t' + ','.join(main)
         out.write(newline.encode('utf8') + '\n')
+        print
+        print
 
 
 if __name__ == '__main__':
